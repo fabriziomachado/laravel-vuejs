5e3b35dcbac438645e632b40018c1ba4cb114542
fabrizio <fabrizio@unesc.net>
2016-11-14 11:44:20 +0000

    altearado nome da app para CodeFin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace CodeFin\Http\Controllers\Auth;
 
-use App\User;
+use CodeFin\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use CodeFin\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
