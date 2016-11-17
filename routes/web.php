373788fd1ae921e7452f37479136887d50b2e0f3
fabrizio <fabrizio@unesc.net>
2016-11-17 21:07:00 +0000

    Instalando dependências do package.json

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 
 use Illuminate\Support\Facades\Gate;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+
 
 Route::get('/user', function () {
     \Illuminate\Support\Facades\Auth::LoginUsingId(2); // logar com o usuario client da silva
@@ -30,7 +21,6 @@ Route::get('/', function () {
      return view('welcome');
      
 });
-
 
 
 Route::get('/home', function(){
