e3e767bd22d2d4b4f72f44d6eb0d7d600b4dce83
fabrizio <fabrizio@unesc.net>
2017-01-20 13:42:52 +0000

    Configurando logout no menu

--- resources/assets/admin/js/components/AdminMenu.vue
+++ resources/assets/admin/js/components/AdminMenu.vue
@@ -1,27 +1,30 @@
 <template>
+    <!-- dropdown list -->
     <ul :id="o.id" class="dropdown-content" v-for="o in config.menusDropdown">
         <li v-for="item in o.items" :class="menuItemClass(item)">
             <a :href="item.url">{{ item.name }}</a>
         </li>
     </ul>
-    <!--<ul id="dropdown-logout" class="dropdown-content">-->
-    <!--    <li>-->
-    <!--        <a :href="config.urlLogout" @click.prevent="goToLogout()">Sair</a>-->
-    <!--        <form id="logout-form" :action="config.urlLogout" method="POST" style="display: none;">-->
-    <!--            <input type="hidden" name="_token" :value="config.csrfToken"/>-->
-    <!--        </form>-->
-    <!--    </li>-->
-    <!--</ul>-->
+    <ul id="dropdown-logout" class="dropdown-content">
+        <li>
+            <a :href="config.urlLogout" @click.prevent="goToLogout()">Sair</a>
+            
+            <form id="logout-form" :action="config.urlLogout" method="POST" style="display: none;">
+                <input type="hidden" name="_token" :value="config.csrfToken"/>
+            </form>
+        </li>
+    </ul>
     
+    <!-- Menu list --> 
     <div class="navbar-fixed">
         <nav class="teal">
             <div class="nav-wrapper container">
-                <!--<div class="col s12">-->
-                    <a href="#" class="right brand-logo hide-on-med-and-down">Code Contas</a>
+                <div class="col s12">
+                    <a href="#" class="left brand-logo hide-on-med-and-down">Code Contas - admin</a>
                     <a href="#" data-activates="nav-mobile" class="button-collapse">
                         <i class="material-icons">menu</i>
                     </a>
-                    <ul class="left hide-on-med-and-down">
+                    <ul class="right hide-on-med-and-down">
                         <li v-for="o in config.menus" :class="menuItemClass(o)">
                             <a v-if="o.dropdownId" class='dropdown-button' href="#!" :data-activates="o.dropdownId">
                                 {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
@@ -34,7 +37,7 @@
                             </a>
                         </li>
                     </ul>
-                <!--</div>-->
+                </div>
                 <ul id="nav-mobile" class="side-nav">
                     <li  v-for="o in config.menus">
                         <a :href="o.url">{{ o.name }}</a>
@@ -64,28 +67,28 @@ export default {
       $('.button-collapse').sideNav()
       $('.dropdown-button').dropdown()
     },
-    // methods:{
-    //     goToLogout(){
-    //         $('#logout-form').submit();
-    //     },
-    //     menuItemClass(menu){
-    //         let menuClass = ['active'];
-    //         if(menu.active){
-    //             return menuClass;
-    //         }
-    //         if(menu.dropdownId != undefined){
-    //             let dropdown = this.config.menusDropdown.find((element) => {
-    //                 return element.id == menu.dropdownId;
-    //             });
-    //             if(dropdown){
-    //                 for(let o of dropdown.items){
-    //                     if(o.active){
-    //                         return menuClass;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    methods:{
+        goToLogout(){
+            $('#logout-form').submit();
+        },
+        // menuItemClass(menu){
+        //     let menuClass = ['active'];
+        //     if(menu.active){
+        //         return menuClass;
+        //     }
+        //     if(menu.dropdownId != undefined){
+        //         let dropdown = this.config.menusDropdown.find((element) => {
+        //             return element.id == menu.dropdownId;
+        //         });
+        //         if(dropdown){
+        //             for(let o of dropdown.items){
+        //                 if(o.active){
+        //                     return menuClass;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+    }
 }
 </script>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,7 +55,7 @@
                         ]
                 ],
                 'urlLogout' => env('URL_ADMIN_LOGOUT'),
-                    'csrfToken' => csrf_token()
+                'csrfToken' => csrf_token()
             ];
             ?>
             
@@ -64,61 +64,61 @@
         @endif
 
 
-        <nav class="navbar navbar-default navbar-static-top teal">
-            <div class="container">
-                <div class="navbar-header">
+        <!--<nav class="navbar navbar-default navbar-static-top teal">-->
+        <!--    <div class="container">-->
+        <!--        <div class="navbar-header">-->
 
                      <!--Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        <!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">-->
+        <!--                <span class="sr-only">Toggle Navigation</span>-->
+        <!--                <span class="icon-bar"></span>-->
+        <!--                <span class="icon-bar"></span>-->
+        <!--                <span class="icon-bar"></span>-->
+        <!--            </button>-->
 
                      <!--Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Code Contas') }}
-                    </a>
-                </div>
+        <!--            <a class="navbar-brand" href="{{ url('/') }}">-->
+        <!--                {{ config('app.name', 'Code Contas') }}-->
+        <!--            </a>-->
+        <!--        </div>-->
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!--        <div class="collapse navbar-collapse" id="app-navbar-collapse">-->
                      <!--Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+        <!--            <ul class="nav navbar-nav">-->
+        <!--                &nbsp;-->
+        <!--            </ul>-->
 
                      <!--Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
+        <!--            <ul class="nav navbar-nav navbar-right">-->
                          <!--Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url(env('URL_ADMIN_LOGIN','/login')) }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+        <!--                @if (Auth::guest())-->
+        <!--                    <li><a href="{{ url(env('URL_ADMIN_LOGIN','/login')) }}">Login</a></li>-->
+        <!--                    <li><a href="{{ url('/register') }}">Register</a></li>-->
+        <!--                @else-->
+        <!--                    <li class="dropdown">-->
+        <!--                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">-->
+        <!--                            {{ Auth::user()->name }} <span class="caret"></span>-->
+        <!--                        </a>-->
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url(env('URL_ADMIN_LOGOUT','/logout')) }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+        <!--                        <ul class="dropdown-menu" role="menu">-->
+        <!--                            <li>-->
+        <!--                                <a href="{{ url(env('URL_ADMIN_LOGOUT','/logout')) }}"-->
+        <!--                                    onclick="event.preventDefault();-->
+        <!--                                             document.getElementById('logout-form').submit();">-->
+        <!--                                    Logout-->
+        <!--                                </a>-->
 
-                                        <form id="logout-form" action="{{ url(env('URL_ADMIN_LOGOUT','/logout')) }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <!--                                <form id="logout-form" action="{{ url(env('URL_ADMIN_LOGOUT','/logout')) }}" method="POST" style="display: none;">-->
+        <!--                                    {{ csrf_field() }}-->
+        <!--                                </form>-->
+        <!--                            </li>-->
+        <!--                        </ul>-->
+        <!--                    </li>-->
+        <!--                @endif-->
+        <!--            </ul>-->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--</nav>-->
         
         </header>
 
